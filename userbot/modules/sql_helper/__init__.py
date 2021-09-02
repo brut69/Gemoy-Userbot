9a12c54eb5446f93ebf3515755a3ac3877051eee
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-03 02:20:45 +0700

    Add files via upload

--- userbot/modules/sql_helper/__init__.py
+++ userbot/modules/sql_helper/__init__.py
@@ -1,25 +1,32 @@
 # Gemoy-Userbot (Telegram Userbot Project )
-
 # Copyright (C) 2021 @dunottagme
 
 # This program is free software: you can redistribute it and/or modify
-
 # it under the terms of the GNU Affero General Public License as
-
 # published by the Free Software Foundation, either version 3 of the
-
 # License, or (at your option) any later version.
 
 # This program is distributed in the hope that it will be useful,
-
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
 # GNU Affero General Public License for more details.
-
 #
-
 # You should have received a copy of the GNU Affero General Public License
-
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from userbot import DB_URI
+
+BASE = declarative_base()
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+SESSION = start()
