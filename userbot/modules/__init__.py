76df118662adc7c0997e214ff94e585dc95f3bfe
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-03 02:14:38 +0700

    Add files via upload

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -1,25 +1,19 @@
-# Gemoy-Userbot (Telegram Userbot Project )
+""" Init file which loads all of the modules """
+from userbot import LOGS
 
-# Copyright (C) 2021 @dunottagme
 
-# This program is free software: you can redistribute it and/or modify
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
 
-# it under the terms of the GNU Affero General Public License as
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
 
-# published by the Free Software Foundation, either version 3 of the
 
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
-# GNU Affero General Public License for more details.
-
-#
-
-# You should have received a copy of the GNU Affero General Public License
-
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("Modules to load: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
