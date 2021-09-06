19b566886882166c6bd012adaea641c84978ad3e
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-06 19:12:17 +0700

    Add files via upload

--- userbot/modules/dbs.py
+++ userbot/modules/dbs.py
@@ -29,5 +29,5 @@ async def amireallydbs(dbs):
     await dbs.edit(""
                    f"**User :** `{DEFAULTUSER}` \n"
                    f"**Status Database:** `{db}`\n"
-                   f"**Geez-Project :** `{BOT_VER}`"
+                   f"**QueenGemoy-Project :** `{BOT_VER}`"
                    "")
