af108f5a36f8ce1f7bcb9fa3abd92d2e0c7cd910
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-30 00:33:14 +0700

    Add files via upload

--- userbot/modules/games.py
+++ userbot/modules/games.py
@@ -16,8 +16,18 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+import math
+import random
+
+import asyncio
+import os
+from asyncio.exceptions import TimeoutError
+
 from userbot import CMD_HELP, bot
 from userbot.events import register
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 
 @register(outgoing=True, pattern=r"^\.xogame(?: |$)(.*)")
@@ -31,7 +41,59 @@ async def _(event):
     tap = await bot.inline_query(botusername, noob)
     await tap[0].click(event.chat_id)
     await event.delete()
+    
+    
+@register(outgoing=True, pattern=r"^\.tetris(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@brugamebot"
+    tetris = "tetris"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, tetris)
+    await tap[0].click(event.chat_id)
+    await event.delete()
 
+
+@register(outgoing=True, pattern=r"^\.puzzle(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@brugamebot"
+    bubble = "bubble"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, bubble)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+    
+    
+@register(outgoing=True, pattern=r"^\.gmbot(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@gamebot"
+    corsairs = "corsairs"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, corsairs)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+    
+    
+    
+@register(outgoing=True, pattern=r"^\.glbot(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@gamebot"
+    lumberjack = "lumberjack"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, lumberjack)
+    await tap[0].click(event.chat_id)
+    await event.delete()
 
 @register(outgoing=True, pattern=r"^\.whisp(?: |$)(.*)")
 async def _(event):
@@ -63,56 +125,116 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    botusername = "@truthordaresbot"
-    honest = "/truth"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.send_message(botusername, honest)
-    await bot.get_response()
-    await tap[0].click(event.chat_id)
-    await event.delete()
-
-
+    link = event.pattern_match.group(1)
+    truth = "truth"
+    await event.edit("```Gib me a second```")
+    async with bot.conversation("@truthordaresbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=774181428))
+            await conv.send_message(f'/truth')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```un-Block @truthordaresbot ploxx```")
+            return
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
+        
+        
 @register(outgoing=True, pattern=r"^\.dare(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    botusername = "@truthordaresbot"
-    dare = "/dare"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.send_message(botusername, dare)
-    await bot.get_response()
-    await tap[0].click(event.chat_id)
-    await event.delete()
+    link = event.pattern_match.group(1)
+    dare = "dare"
+    await event.edit("```Gib me a second```")
+    async with bot.conversation("@truthordaresbot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=774181428))
+            await conv.send_message(f'/dare')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```un-Block @truthordaresbot ploxx```")
+            return
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
 
 
 @register(outgoing=True, pattern=r"^\.spill(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    botusername = "@Spillgame_bot"
-    spill = "/spill"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.send_message(botusername, spill)
-    await bot.get_response()
-    await tap[0].click(event.chat_id)
-    await event.delete()
+    link = event.pattern_match.group(1)
+    spill = "spill"
+    await event.edit("```Gib me a second```")
+    async with bot.conversation("@Spillgame_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=774181428))
+            await conv.send_message(f'/spill ')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```un-Block @Spillgame_bot ploxx```")
+            return
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
 
 
-@register(outgoing=True, pattern=r"^\.f100(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.fmulai(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    botusername = "@Familys100_bot"
-    family = "/next"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.send_message(botusername, family)
-    await bot.get_response()
-    await tap[0].click(event.chat_id)
-    await event.delete()
+    link = event.pattern_match.group(1)
+    mulaifamily = "mulaifamily"
+    await event.edit("```Gib me a second```")
+    async with bot.conversation("@Familys100_bot ") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=774181428))
+            await conv.send_message(f'/mulai')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```un-Block @Familys100_bot ploxx```")
+            return
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
+            
+            
+@register(outgoing=True, pattern=r"^\.fnext(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    nextfamily = "nextfamily"
+    await event.edit("```Gib me a second```")
+    async with bot.conversation("@Familys100_bot ") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=774181428))
+            await conv.send_message(f'/next')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```un-Block @Familys100_bot ploxx```")
+            return
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
+
 
 
 CMD_HELP.update({
@@ -123,17 +245,33 @@ CMD_HELP.update({
 \nPenggunaan: Dapatkan applikasi mod\
 \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.whisp <teks> <username/ID>`\
 \nPenggunaan: Berikan pesan rahasia."
-})
+    })
+
 
 
 CMD_HELP.update({
-    "games2": "\
+    "games1": "\
 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.truth`\
 \nPenggunaan: Tantangan kejujuran.\
 \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.dare`\
 \nPenggunaan: Tantangan lain.\
 \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.spill`\
 \nPenggunaan: Spill pertanyaan.\
-\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.f100`\
+\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fmulai`\
+\nPenggunaan: Kuis Family 100.\
+\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fnext`\
 \nPenggunaan: Kuis Family 100."
-})
+    })
+    
+    
+    CMD_HELP.update({
+    "games2": "\
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.tetris`\
+\nPenggunaan: Jadul Game.\
+\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.puzzle`\
+\nPenggunaan: Idem atas weh.\
+\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gmbot`\
+\nPenggunaan: Lets play bruh.\
+\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.glbot`\
+\nPenggunaan: Play bae."
+    })
