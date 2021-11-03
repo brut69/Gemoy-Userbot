5b55f73351d73b84f857c0299809626cd1c32dee
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-11-04 00:38:52 +0700

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -28,7 +28,8 @@ from distutils.util import strtobool as sb
 from math import ceil
 
 from pylast import LastFMNetwork, md5
-from pySmartDL import SmartDL
+from pySmartDL import SmartDLSmartDL
+from pytgcalls import PyTgCalls
 from pymongo import MongoClient
 from datetime import datetime
 from redis import StrictRedis
@@ -330,11 +331,22 @@ for binary, path in binaries.items():
 
 # 'bot' variable
 if STRING_SESSION:
-    # pylint: disable=invalid-name
-    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+    session = StringSession(str(STRING_SESSION))
 else:
-    # pylint: disable=invalid-name
-    bot = TelegramClient("userbot", API_KEY, API_HASH)
+    session = "GemoyUserbot"
+try:
+    bot = TelegramClient(
+        session=session,
+        api_id=API_KEY,
+        api_hash=API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"STRING_SESSION - {e}")
+    sys.exit()
+call_py = PyTgCalls(bot)
 
 
 async def check_botlog_chatid():
