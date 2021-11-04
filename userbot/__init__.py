84297bccfb22b3a213acd1cfd98ea549673d4b12
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-11-04 13:57:23 +0700

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -339,9 +339,6 @@ try:
         session=session,
         api_id=API_KEY,
         api_hash=API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
     )
 except Exception as e:
     print(f"STRING_SESSION - {e}")
